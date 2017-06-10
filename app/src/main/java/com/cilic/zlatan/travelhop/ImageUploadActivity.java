49318b918eae998bcf990287da2b9399ce59b676
zlatancilic <zlatancilic@hotmail.com>
2017-06-10 16:35:59 +0200

    Added uploading images to FireBase

--- app/src/main/java/com/cilic/zlatan/travelhop/ImageUploadActivity.java
+++ app/src/main/java/com/cilic/zlatan/travelhop/ImageUploadActivity.java
@@ -4,6 +4,7 @@ import android.*;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
@@ -13,14 +14,40 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageUploadActivity extends AppCompatActivity {
 
     ImageView imageView;
     Button openGalleryButton;
     Button openCameraButton;
+    Button uploadImageButton;
+    EditText captionEditText;
+    EditText locationEditText;
+
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+    private static final SimpleDateFormat creation_date_format = new SimpleDateFormat("MM.dd.yyyy HH:mm:ss");
 
     private final int REQUEST_CAMERA_PERMISSION = 111;
     private final int REQUEST_GALLERY_PERMISSION = 222;
@@ -32,6 +59,24 @@ public class ImageUploadActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.selected_image_imageview);
         openGalleryButton = (Button) findViewById(R.id.gallery_button);
+        openCameraButton = (Button) findViewById(R.id.camera_button);
+        uploadImageButton = (Button) findViewById(R.id.upload_image_button);
+        captionEditText = (EditText) findViewById(R.id.caption_edittext);
+        locationEditText = (EditText) findViewById(R.id.location_edittext);
+
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmapImage = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                if(bitmapImage != null) {
+                    uploadImage(bitmapImage);
+                }
+                else {
+                    Toast.makeText(ImageUploadActivity.this, "You have not selected an image yet", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+
         openGalleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,7 +89,6 @@ public class ImageUploadActivity extends AppCompatActivity {
             }
         });
 
-        openCameraButton = (Button) findViewById(R.id.camera_button);
         openCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,6 +111,8 @@ public class ImageUploadActivity extends AppCompatActivity {
                 openGalleryButton.callOnClick();
             }
         }
+
+
     }
 
     @Override
@@ -127,5 +173,38 @@ public class ImageUploadActivity extends AppCompatActivity {
                 Toast.makeText(ImageUploadActivity.this, "Permission not granted", Toast.LENGTH_SHORT);
             }
         }
+    }
+
+    private void uploadImage(Bitmap image) {
+        final String caption = captionEditText.getText().toString();
+        final String location = locationEditText.getText().toString();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        final String img_path = "activityStreamImages/" + sdf.format(timestamp) + "_" + firebaseAuth.getCurrentUser().getUid();
+
+        StorageReference imageRef = storageReference.child(img_path);
+        UploadTask uploadTask = imageRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(ImageUploadActivity.this, "ERROR WITH UPLOAD", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("downloadPath", img_path);
+                map.put("caption", caption);
+                map.put("location", location);
+                map.put("dateCreated", creation_date_format.format(timestamp));
+                String key = databaseReference.child("activityStreamPosts").push().getKey();
+                databaseReference.child("activityStreamPosts").child(firebaseAuth.getCurrentUser().getUid()).child(key).setValue(map);
+
+                Toast.makeText(ImageUploadActivity.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
